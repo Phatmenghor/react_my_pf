f89b20bb01d4c8773a668683bbb326fdb395c316
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-05-21 11:13:59 +0700

    push

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ const ComputersCanvas = () => {
   return (
     <div className="absolute   w-full h-full flex justify-center  items-end xl:justify-end  xl:items-start">
       <img
-        className="w-[100%] h-[60%] sm:w-[80%] sm:h-[60%] md:w-[70%] md:h-[70%] lg:w-[50%] lg:h-[70%] xl:w-[48%] xl:h-[75%] 2xl:w-[45%] 2xl:h-[86%]"
+        className="w-[100%] h-[60%] sm:w-[80%] sm:h-[60%] md:w-[70%] md:h-[70%] lg:w-[55%] lg:h-[70%] xl:w-[55%] xl:h-[85%] 2xl:w-[40%] 2xl:h-[86%]"
         src={me}
         alt="menghor phat"
       />
