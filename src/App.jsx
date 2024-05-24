695d3f6f9b049e53fc0015abe10102604a4bb258
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-05-24 18:12:19 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Video />
+        {/* <Video /> */}
         {/* <Feedbacks /> */}
 
         <div className="relative z-0">
